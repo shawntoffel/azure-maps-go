0a8907b21c5548ca5a29bf8a5f52a4b2ec130e29
Shawn Toffel <shawn.toffel@gmail.com>
2021-09-07 03:58:22 +0000

    WindSpeed -> WeatherUnit

--- weather/entities/wind.go
+++ weather/entities/wind.go
@@ -2,16 +2,10 @@ package entities
 
 type Wind struct {
 	Direction *WindDirection `json:"direction,omitempty"`
-	Speed     *WindSpeed     `json:"speed,omitempty"`
+	Speed     *WeatherUnit   `json:"speed,omitempty"`
 }
 
 type WindDirection struct {
 	Degrees              float64 `json:"degrees"`
 	LocalizedDescription string  `json:"localizedDescription,omitempty"`
-}
-
-type WindSpeed struct {
-	Unit     string  `json:"unit,omitempty"`
-	UnitType int     `json:"unitType"`
-	Value    float64 `json:"value"`
 }
